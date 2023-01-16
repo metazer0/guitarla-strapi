af026e4fcd6e08d6d7c1d76b79ba2daf5efa5279
Julio David <isaacdvid15.96@outlook.com>
2023-01-16 10:20:43 -0600

    update strapi

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','1bd5744306a4c0c7f9dbc8c07f170899'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', '1bd5744306a4c0c7f9dbc8c07f170899'),
   },
 });
